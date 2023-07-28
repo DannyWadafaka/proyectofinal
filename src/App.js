2d3c7ba8f05fc3e5a51cf62ff41076ccffabe5c8
DannyWadafaka <dalejoh@unal.edu.co>
2023-07-28 14:39:39 -0500

    webada

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import Home from './pages/Home';
+import Login from './auth/Login';
+import Logout from './auth/Logout';
+import { Link, Route, Routes } from 'react-router-dom';
+import { Favorites } from './pages/Favorites';
+import SearchBar from './components/SearchBar';
+
+
+function App() {
+  const {isAuthenticated} = useAuth0();
+  return (
+    <div className="App">
+      <header className="App-header">
+      {isAuthenticated ? (
+        <>
+        <nav>
+        <Link to='/favorites'> Favoritos</Link>
+        <Link to='/home'> Home </Link>
+        <Link to='/logout'> logout </Link>
+        </nav>
+        </>
+      ): (
+        <Login/>
+      )}
+      </header>
+      <Routes>
+        <Route path='/favorites' element={<Favorites/>}/>
+        <Route path='/home' element={<SearchBar/>}/>
+        <Route path='/logout' element={<Logout/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/conf_auth.js
@@ -0,0 +1,12 @@
+
+const conf_auth = {
+    domain: 'dannnnnyujuyei.us.auth0.com',
+    clientId: 'gy09qU7zQ2ZztdBwvAEkMRbbdHCHI0By',
+    redirectUri: window.location.origin
+}
+
+export default conf_auth
+
+
+
+//y7MkA5f8rxvSNSpZFoX2LxLT05XSKwR7
